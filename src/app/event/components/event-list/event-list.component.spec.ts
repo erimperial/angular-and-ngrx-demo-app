aaee057c2aa4fc78b2c91f30aaa2862b7db64477
Erika Jane Imperial <imperial.erikajane@gmail.com>
2023-05-10 18:47:55 +0800

    Added working configuration of Jest unit testing

--- src/app/event/components/event-list/event-list.component.spec.ts
+++ src/app/event/components/event-list/event-list.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EventListComponent } from './event-list.component';
+import { Attendee } from 'src/app/models';
 
 describe('EventListComponent', () => {
   let component: EventListComponent;
@@ -8,9 +9,8 @@ describe('EventListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ EventListComponent ]
-    })
-    .compileComponents();
+      declarations: [EventListComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(EventListComponent);
     component = fixture.componentInstance;
@@ -19,5 +19,19 @@ describe('EventListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have no attendee on load', () => {
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should have 1 attendee on load', () => {
+    component.attendees = [
+      { name: 'Duncan', attending: true, guests: 2 },
+    ] as Attendee[];
+
+    fixture.detectChanges();
+
+    expect(component).toMatchSnapshot();
   });
 });
--- src/app/event/components/event-list/event-list.component.ts
+++ src/app/event/components/event-list/event-list.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Attendee } from 'src/app/models';
 
 @Component({
   selector: 'app-event-list',
   templateUrl: './event-list.component.html',
-  styleUrls: ['./event-list.component.scss']
+  styleUrls: ['./event-list.component.scss'],
 })
-export class EventListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class EventListComponent {
+  @Input() attendees: Attendee[] = [];
 }
